1ff03430c935724ef892ec2f0916549b16c81de4
juanjerrah <juanjerrah15@gmail.com>
2025-08-29 18:44:55 -0300

    feat: setting up env variables

---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import "os"
+
+type Config struct {
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassword string
+	DbName     string
+	ServerPort string
+}
+
+func LoadConfig() *Config {
+	return &Config{
+		DbHost:     getEnv("DB_HOST", "localhost"),
+		DbPort:     getEnv("DB_PORT", "5432"),
+		DbUser:     getEnv("DB_USER", "postgres"),
+		DbPassword: getEnv("DB_PASSWORD", "1234"),
+		DbName:     getEnv("DB_NAME", "dbname"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+}
+
+func getEnv(key string, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
